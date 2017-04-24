c312c3f16ceb690d946998aabc03b5e13e7b92b0
ómar yamak <yamak.omar@gmail.com>
2017-04-24 13:47:07 +0000

    bla

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let backArrow = document.getElementById('arrow');
 let canvas = $(".canvasContainer").children("canvas");
 
 
-console.log("hallo albertgith ");
+console.log("hallo ");
 
 console.log("hallo albertgit ");
 console.log("hallo omar ");
