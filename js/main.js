bf6fe402125702cd0031a1eac4b610d16cf87e53
Albert Gudlaugsson <albertgudlaugsson@Alberts-MacBook-Pro.local>
2017-04-24 11:44:05 +0000

    Byrjunarstig

---
+++ js/main.js
@@ -0,0 +1,97 @@
+let play = document.getElementById('play');
+let howToPlayList = document.getElementById('howToPlayList');
+let aboutUsList = document.getElementById('aboutUsList');
+
+let welcomeLeft = document.getElementById('welcomeLeft');
+let welcomeRight = document.getElementById('welcomeRight');
+let backArrow = document.getElementById('arrow');
+
+let canvas = $(".canvasContainer").children("canvas");
+
+const toggleListClass = (parentDivID) => {
+    let sidemenuHowtoplay = document.getElementById(parentDivID);
+    let sidemenuList = sidemenuHowtoplay.childNodes;
+    let i = 1;
+    counter = 2;
+    setInterval(function() {
+        if (i < sidemenuList.length) {
+            sidemenuList[i].classList.toggle("activeList");
+            i = i + counter;
+            sidemenuList.length
+            console.log(sidemenuList.length)
+            console.log(i)
+        } else {
+            clearInterval();
+        }
+    }, 150)
+};
+
+play.addEventListener("click", function() {
+    welcomeLeft.style.left = "-100%";
+    welcomeRight.style.right = "-100%";
+    threeJSPlayground.classList.toggle("active-ingame");
+});
+
+let howToPlay = document.getElementById('howToPlay');
+let howToPlayLeft = document.getElementById('howToPlayLeft')
+let howToPlayRight = document.getElementById('howToPlayRight')
+let carpenter = document.getElementById('carpenter');
+
+howToPlayList.addEventListener("click", function() {
+    welcomeLeft.style.bottom = "-100%";
+    welcomeRight.style.top= "-100%";
+    howToPlayLeft.style.top = "0%";
+    howToPlayRight.style.bottom = "0%";
+    howToPlay.style.zIndex = "10";
+    howToPlay.className += " active"
+    backArrow.style.left = "20px";
+    carpenter.classList.toggle("animation-carpenter");
+    toggleListClass('sidemenu-howtoplay-content');
+});
+
+let aboutUs = document.getElementById('aboutUs');
+let aboutUsLeft = document.getElementById('aboutUsLeft')
+let aboutUsRight = document.getElementById('aboutUsRight')
+
+aboutUsList.addEventListener("click", function() {
+    welcomeLeft.style.bottom = "100%";
+    welcomeRight.style.top= "100%";
+    aboutUsLeft.style.bottom = "0%";
+    aboutUsRight.style.top= "0%";
+    aboutUs.style.zIndex = "10";
+    aboutUs.className += " active";
+    backArrow.style.left = "20px";
+    toggleListClass("aboutUsRight");
+
+})
+
+backArrow.addEventListener("click", function() {
+    if (aboutUs.classList.contains("active")) {
+        aboutUs.classList.remove("active");
+        setTimeout(function() {
+            aboutUsLeft.style.bottom = "-100%";
+            aboutUsRight.style.top= "-100%";
+            aboutUs.style.zIndex = "-1";
+            aboutUs.classList.remove("active");
+            backArrow.style.left = "-40px";
+            welcomeLeft.style.bottom = "0%";
+            welcomeRight.style.top= "0%";
+        }, 0)
+    } else if (howToPlay.classList.contains("active")){
+        howToPlayLeft.style.top = "-100%";
+        howToPlayRight.style.bottom = "-100%";
+        howToPlay.style.zIndex = "-1";
+        howToPlay.classList.remove("active");
+        backArrow.style.left = "-40px";
+        welcomeLeft.style.bottom = "0%";
+        welcomeRight.style.top= "0%";
+        carpenter.classList.remove("animation-carpenter");
+        toggleListClass('sidemenu-howtoplay-content');
+    }
+});
+
+// INGAME JS
+
+$('.ingame-sidemenu').click(function() {
+      $('.nav').toggleClass("left");
+});
---
+++ js/threejs.js
@@ -0,0 +1,132 @@
+let container;
+let camera, scene, renderer;
+let plane, cube;
+let mouse, raycaster, isShiftDown = false;
+let rollOverMesh, rollOverMaterial;
+let cubeGeo, cubeMaterial;
+let objects = [];
+let threeJSPlayground = document.getElementById('playground');
+init();
+render();
+function init() {
+
+
+  container = document.createElement( 'div' );
+  container.className = "canvasContainer";
+  threeJSPlayground.appendChild(container)
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.set( 500, 800, 1300 );
+  camera.lookAt( new THREE.Vector3() );
+  scene = new THREE.Scene();
+  // roll-over helpers
+  rollOverGeo = new THREE.BoxGeometry( 50, 50, 50 );
+  rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+  rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
+  scene.add( rollOverMesh );
+  // cubes
+  cubeGeo = new THREE.BoxGeometry( 50, 50, 50 );
+  cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c, map: new THREE.TextureLoader().load("kalli1.jpg") } );
+  // grid
+  var size = 5000, step = 50;
+  var geometry = new THREE.Geometry();
+  for ( var i = - size; i <= size; i += step ) {
+    geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+    geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+    geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+    geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+  }
+  var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
+  var line = new THREE.LineSegments( geometry, material );
+  scene.add( line );
+  //
+  raycaster = new THREE.Raycaster();
+  mouse = new THREE.Vector2();
+  var geometry = new THREE.PlaneBufferGeometry( 50000, 50000 );
+  geometry.rotateX( - Math.PI / 2 );
+  plane = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { visible: false } ) );
+  scene.add( plane );
+  objects.push( plane );
+  // Lights
+  var ambientLight = new THREE.AmbientLight( 0x606060 );
+  scene.add( ambientLight );
+  var directionalLight = new THREE.DirectionalLight( 0xffffff );
+  directionalLight.position.set( 1, 0.75, 0.5 ).normalize();
+  scene.add( directionalLight );
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setClearColor( 0xf0f0f0 );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  container.appendChild( renderer.domElement );
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+  document.addEventListener( 'keydown', onDocumentKeyDown, false );
+  document.addEventListener( 'keyup', onDocumentKeyUp, false );
+  //
+  window.addEventListener( 'resize', onWindowResize, false );
+  document.addEventListener('mousedown', onDocumentMouseDown, false);
+  render();
+}
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+function onDocumentMouseMove( event ) {
+  event.preventDefault();
+  mouse.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+  raycaster.setFromCamera( mouse, camera );
+  var intersects = raycaster.intersectObjects( objects );
+  if ( intersects.length > 0 ) {
+    var intersect = intersects[ 0 ];
+    rollOverMesh.position.copy( intersect.point ).add( intersect.face.normal );
+    rollOverMesh.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+  }
+  render();
+}
+function onDocumentMouseDown( event ) {
+  event.preventDefault();
+  mouse.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+  raycaster.setFromCamera( mouse, camera );
+  var intersects = raycaster.intersectObjects( objects );
+  if ( intersects.length > 0 ) {
+    var intersect = intersects[ 0 ];
+    // delete cube
+    if ( isShiftDown ) {
+      if ( intersect.object != plane ) {
+        scene.remove( intersect.object );
+        objects.splice( objects.indexOf( intersect.object ), 1 );
+      }
+    // create cube
+    } else {
+      var voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
+      voxel.position.copy( intersect.point ).add( intersect.face.normal );
+      voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+      scene.add( voxel );
+      objects.push( voxel );
+    }
+    render();
+  }
+}
+function onDocumentKeyDown( event ) {
+  switch( event.keyCode ) {
+    case 16: isShiftDown = true; break;
+  }
+}
+function onDocumentKeyUp( event ) {
+  switch ( event.keyCode ) {
+    case 16: isShiftDown = false; break;
+  }
+}
+function render() {
+  renderer.render( scene, camera );
+}
+
+function onDocumentKeyDown(){
+  animate();
+}
+
+function animate() {
+cube.rotation.y += 500;
+render();
+
+}
